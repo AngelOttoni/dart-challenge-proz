c33b9a2cdcee4b19054adf8ef9f3cdf444263db9
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-07 19:59:30 -0300

    initial project structure

---
+++ bin/dart_challenge.dart
@@ -0,0 +1,5 @@
+import 'package:dart_challenge/dart_challenge.dart' as dart_challenge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_challenge.calculate()}!');
+}
---
+++ test/dart_challenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_challenge/dart_challenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
