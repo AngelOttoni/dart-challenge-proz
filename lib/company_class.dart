57577f4ead0d2a2607b22b73e3c26a670095d7ee
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-13 16:58:37 -0300

    Updated Company class

--- lib/company_class.dart
+++ lib/company_class.dart
@@ -4,13 +4,18 @@ import 'package:dart_challenge/partner_class.dart';
 class Company extends LegalPerson {
   final id = Uuid().v4();
   final registrationDate = DateTime.now();
-  String phone;
-  Company(cnpj, corporateName, tradeName, this.phone, address)
-      : super(cnpj, corporateName, tradeName, address);
+  String _phone;
+  Company(id, _cnpj, _corporateName, _tradeName, this._phone, address)
+      : super(_cnpj, _corporateName, _tradeName, address);
 
-  void newCompany() {}
-  void searchCompanyCnpj() {}
-  void searchCompanyMemberCpfCnpj() {}
-  void listCompanies() {}
-  void delCompanyId() {}
+  //Getters
+
+  //Setters
+
+  
+  // void newCompany() {}
+  // void searchCompanyCnpj() {}
+  // void searchCompanyMemberCpfCnpj() {}
+  // void listCompanies() {}
+  // void delCompanyId() {}
 }
