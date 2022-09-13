3024673910ab3857e35a023ac7cdb681b6d2810f
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-13 16:20:10 -0300

    Address class created and method implemented

---
+++ lib/address_class.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+class Address {
+  late String street;
+  late int number;
+  late String complement;
+  late String district;
+  late String city;
+  late String state;
+  late String cep;
+  Address(
+      {required this.street,
+      required this.number,
+      required this.complement,
+      required this.district,
+      required this.state,
+      required this.cep});
+
+  @override
+  String toString() {
+    return "Street $street, $number, $complement, $district, $city/$state, CEP $cep";
+  }
+}
