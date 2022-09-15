3f8a5eb8da1a56dca0e79c4cc85b548359e88cc5
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-15 15:20:09 -0300

    object instantiation

--- bin/dart_challenge.dart
+++ bin/dart_challenge.dart
@@ -1,13 +1,86 @@
 // import 'package:dart_challenge/dart_challenge.dart' as dart_challenge;
 import 'package:dart_challenge/address_class.dart';
 import 'package:dart_challenge/company_class.dart';
+import 'package:dart_challenge/main_menu.dart';
 import 'package:dart_challenge/partner_class.dart';
 
 // void main(List<String> arguments) {
 //   print('Hello world: ${dart_challenge.calculate()}!');
 // }
 
-
 void main(List<String> args) {
-  //
+//-----------------------------------------------------------------------------
+  Address address1 = Address(
+      street: "Rua Três",
+      number: 338,
+      complement: "",
+      district: "Vila Industrial",
+      city: "Piracicaba",
+      state: "SP",
+      cep: "13.412-233");
+
+  Address addressPartner1 = Address(
+      street: "Rua Ulisses Bueno",
+      number: 175,
+      complement: "",
+      district: "Vila Rosa",
+      city: "Goiânia",
+      state: "GO",
+      cep: "74.935-870");
+
+  NaturalPerson partner1 = NaturalPerson(
+      "763.679.800-08", "Mirella Kamilly Letícia Barbosa", addressPartner1);
+
+  Company;
+  LegalPerson company1 = Company(
+      "",
+      "11.941.838/0001-04",
+      "",
+      "Elisa e Flávia Pães e Doces ME",
+      "Pães e Doces",
+      "(19) 9 8838-0630",
+      address1,
+      partner1);
+  print(company1.toString());
+  print("");
+//-----------------------------------------------------------------------------
+  Address address2 = Address(
+      street: "Rua Domingos Vendemiati",
+      number: 154,
+      complement: "",
+      district: "Parque Recanto do Parrilho",
+      city: ", Jundiaí",
+      state: "SP",
+      cep: "13.219-051");
+
+  Address addressPartner2 = Address(
+      street: "Rua Ângelo Alberto Nesti",
+      number: 639,
+      complement: "",
+      district: "Bussocaba",
+      city: "Osasco",
+      state: "SP",
+      cep: "06.053-060");
+
+  LegalPerson partner2 = LegalPerson(
+      "87.009.263/0001-40",
+      "Lavínia e Tereza Locações de Automóveis Ltda",
+      "Loc. Auto",
+      addressPartner2);
+
+  Company;
+  LegalPerson company2 = Company(
+      "",
+      "92.456.516/0001-63",
+      "",
+      "Arthur e Andreia Telas Ltda",
+      "Telas",
+      "(11) 9 9572-6906",
+      address2,
+      partner2);
+  print(company2.toString());
+
+  //--------------------------------------------------------------------------
+  
+  mainMenu();
 }
