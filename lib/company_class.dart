379655469d8609f53a7a46d4d21708388b1a1d2a
AngelOttoni <angel_m_ottoni@hotmail.com>
2022-09-15 15:13:21 -0300

    get, partner e toString added

--- lib/company_class.dart
+++ lib/company_class.dart
@@ -1,18 +1,29 @@
 import 'package:uuid/uuid.dart';
 import 'package:dart_challenge/partner_class.dart';
+//import 'package:dart_challenge/registrationsystem_class.dart';
 
 class Company extends LegalPerson {
-  final id = Uuid().v4();
+  String id = Uuid().v4();
   final registrationDate = DateTime.now();
   String _phone;
-  Company(id, _cnpj, _corporateName, _tradeName, this._phone, address)
+  Partner partner;
+
+  
+  Company(id, _cnpj, registrationDate, _corporateName, _tradeName, this._phone,
+      address, this.partner)
       : super(_cnpj, _corporateName, _tradeName, address);
 
   //Getters
-
+  String get getPhone {
+    return _phone;
+  }
   //Setters
 
-  
+  @override
+  String toString() {
+    return "Company Id: $id,\n CNPJ: $getCnpj,\n Registration Date: $registrationDate,\n Company: $getCorporateName,\n Trade Name: $getTradeName,\n Phone: $getPhone,\n Patner:\n ${partner.toString()}"; //como inserir o sócio? resolvido
+  }
+
   // void newCompany() {}
   // void searchCompanyCnpj() {}
   // void searchCompanyMemberCpfCnpj() {}
